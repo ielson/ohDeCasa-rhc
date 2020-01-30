29501eb900490cb66650f61148950cc6d0a4d7ec
ielson <danmascandrade@gmail.com>
2020-01-29 23:06:02 -0300

    added checkout function

--- ohDeCasa-api.py
+++ ohDeCasa-api.py
@@ -4,24 +4,21 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-raulOpen = False
 usersFile = "activeUsers.txt"
 checkedUsers = {}
 
 @app.route("/")
 def index():
-    return "RHC - ô de casa homepage"
+    return "RHC - Ô de casa homepage"
 
 @app.route("/status")
 def status():
-    usersAvailable = checkIfUsersAvailable()
-    print(usersAvailable)
-    return usersAvailable
+    availableUsers = checkIfUsersAvailable()
+    return availableUsers
 
 @app.route("/checkin", methods=['POST'])
 #TODO deal with timezones
 def checkin():
-    raulOpen = True
     checkIfUsersAvailable()
     try:
         reqData = request.get_json()
@@ -29,17 +26,28 @@ def checkin():
     except KeyError:
         user = "Raul Seixas"
     currentTime = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
-    checkedUser = {'user': user, 'datetime': currentTime, 'isThere': raulOpen}
+    checkedUser = {'user': user, 'datetime': currentTime, 'isThere': True}
     checkedUsers[user] = checkedUser
-    checkedUsers['isOpen'] = raulOpen
     with open(usersFile, 'w') as file:
         json.dump(checkedUsers, file)
     return checkedUser
 
 
-@app.route("/checkout")
+@app.route("/checkout", methods=['POST'])
 def checkout():
-    pass
+    global checkedUsers
+    checkIfUsersAvailable()
+    try:
+        reqData = request.get_json()
+        user = reqData['user']
+        del checkedUsers[user]
+        with open(usersFile, 'w') as file:
+            json.dump(checkedUsers, file)
+        currentTime = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+        checkedUser = {'user': user, 'datetime': currentTime, 'isThere': False}
+        return checkedUser
+    except KeyError:
+        return "User was not checked-in, could not check-out"
 
 
 def checkIfUsersAvailable():
@@ -48,6 +56,14 @@ def checkIfUsersAvailable():
         if not checkedUsers:
             with open(usersFile, 'r') as file:
                 checkedUsers = json.load(file)
+        print(len(checkedUsers))
+        if len(checkedUsers) > 1:
+            raulOpen = True
+            print("raul open")
+        else:
+            raulOpen = False
+            print('raul closed ')
+        checkedUsers['isOpen'] = raulOpen
         return checkedUsers
     except FileNotFoundError:
         return "No user ever checked in, please be the first sending a POST to /checkin"
