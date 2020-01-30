630d0ee20609a028add67a0c55f9224abf8238db
ielson <danmascandrade@gmail.com>
2020-01-29 22:18:40 -0300

    added status function

--- ohDeCasa-api.py
+++ ohDeCasa-api.py
@@ -14,27 +14,40 @@ def index():
 
 @app.route("/status")
 def status():
-    return 'status'
+    usersAvailable = checkIfUsersAvailable()
+    print(usersAvailable)
+    return usersAvailable
 
 @app.route("/checkin", methods=['POST'])
 #TODO deal with timezones
-#TODO save in file
 def checkin():
     raulOpen = True
+    checkIfUsersAvailable()
     try:
         reqData = request.get_json()
         user = reqData['user']
     except KeyError:
         user = "Raul Seixas"
     currentTime = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
-    checkedUser = {'user': user, 'datetime': currentTime, 'open': raulOpen}
+    checkedUser = {'user': user, 'datetime': currentTime, 'isThere': raulOpen}
     checkedUsers[user] = checkedUser
+    checkedUsers['isOpen'] = raulOpen
     with open(usersFile, 'w') as file:
         json.dump(checkedUsers, file)
     return checkedUser
 
 
-
 @app.route("/checkout")
 def checkout():
     pass
+
+
+def checkIfUsersAvailable():
+    global checkedUsers
+    try:
+        if not checkedUsers:
+            with open(usersFile, 'r') as file:
+                checkedUsers = json.load(file)
+        return checkedUsers
+    except FileNotFoundError:
+        return "No user ever checked in, please be the first sending a POST to /checkin"
